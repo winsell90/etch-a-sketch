ca333143ea7ebe58af77d8fb854d3f174963e42d
Warren Insell <insell.warren@gmail.com>
2022-04-08 09:15:43 -0700

    Made for of loop to transition each box on hover

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let container = document.querySelector('.container');
 
+
 for (let i = 1; i < 257; i++) {
 let square = document.createElement('div');
+square.classList.add('squares');
 square.style.cssText = "border: 1px solid black; height: auto; width: 100%";
 container.appendChild(square)
+}
+
+square1 = document.getElementsByClassName('squares');
+
+for (let squares of square1) {
+squares.addEventListener('mouseenter', () => {
+  squares.classList.add('etch');
+})
 }
